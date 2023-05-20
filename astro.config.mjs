20dab9c18bc53b69b282053f88a73d36e6d0609d
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2023-05-20 14:59:26 +0900

    add: netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import vercel from "@astrojs/vercel/serverless";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel()
+  adapter: netlify(),
+  adapter: vercel(),
 });
