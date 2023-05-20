87de933e0be82dafb3e4b72a1041097062e01efe
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2023-05-20 15:03:07 +0900

    add: try hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from "astro/config";
 
 import vercel from "@astrojs/vercel/serverless";
-import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: netlify(),
+  output: "hybrid",
   adapter: vercel(),
 });
